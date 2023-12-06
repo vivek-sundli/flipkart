3c03f2af25ea2c2a13bc97a5577e8d4be646fd87
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2023-12-06 21:32:33 +0530

    Delete product-service/product-service directory

--- product-service/product-service/src/main/java/com/project/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.productservice.repository;
-
-import com.project.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product,String> {
-}
