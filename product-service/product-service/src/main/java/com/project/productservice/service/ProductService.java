4cca9f039bf78b4914241da161d91b4c1d7df814
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2023-12-06 21:31:43 +0530

    product catalog
    working product catalog CRUD

---
+++ product-service/product-service/src/main/java/com/project/productservice/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.project.productservice.service;
+
+import com.project.productservice.controller.dto.ProductRequest;
+import com.project.productservice.controller.dto.ProductResponse;
+import com.project.productservice.model.Product;
+import com.project.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+      log.info("Product "+product.getId()+" Saved!");
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .build();
+    }
+}
